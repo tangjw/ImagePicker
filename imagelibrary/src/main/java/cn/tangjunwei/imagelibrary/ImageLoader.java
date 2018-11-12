77888762d6841acb32e6af3088849e11c4f221d4
tangjw <tjwms@outlook.com>
2018-11-12 23:17:04 +0800

    ImageLoader

---
+++ imagelibrary/src/main/java/cn/tangjunwei/imagelibrary/ImageLoader.java
@@ -0,0 +1,26 @@
+package cn.tangjunwei.imagelibrary;
+
+import android.app.Activity;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * desc
+ * <p>
+ * Created by tangjunwei on 2018/11/12.
+ * <a href="mailto:tjwabc@gmail.com">Contact me</a>
+ * <a href="https://github.com/tangjw">Follow me</a>
+ */
+public interface ImageLoader {
+//    GlideApp.with(context)
+//            .load(mList.get(position).path)
+//            .placeholder(R.drawable.image_placeholder)
+//            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+//            .transition(DrawableTransitionOptions.withCrossFade())
+//            .into(viewHolder.imageView);
+    
+    void loadImage(Activity activity, String path, ImageView imageView);
+    
+    void loadImage(Fragment fragment, String path, ImageView imageView);
+}
---
+++ imagelibrary/src/main/java/cn/tangjunwei/imagelibrary/ImageLoaderImpl.java
@@ -0,0 +1,26 @@
+package cn.tangjunwei.imagelibrary;
+
+import android.app.Activity;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * desc
+ * <p>
+ * Created by tangjunwei on 2018/11/12.
+ * <a href="mailto:tjwabc@gmail.com">Contact me</a>
+ * <a href="https://github.com/tangjw">Follow me</a>
+ */
+public class ImageLoaderImpl implements ImageLoader {
+    
+    @Override
+    public void loadImage(Activity activity, String path, ImageView imageView) {
+        
+    }
+    
+    @Override
+    public void loadImage(Fragment fragment, String path, ImageView imageView) {
+        
+    }
+}
